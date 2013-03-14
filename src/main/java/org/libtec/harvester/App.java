1bd29eb1fddbf8fa8b69615bf81db0ba3cb5c38d
Jochen Schirrwagen <jochen.schirrwagen@uni-bielefeld.de>
2013-03-14 17:55:31 +0100

    Initial commit

---
+++ src/main/java/org/libtec/harvester/App.java
@@ -0,0 +1,13 @@
+package org.libtec.harvester;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
